8a390693927bca40c37d3dea077d610cfd144033
Bobur_Zokirov <zokirovbobur93@gmail.com>
2021-10-21 12:05:38 +0500

    2nd task is ready to review

---
+++ src/main/java/com/epam/advancedMultithreading/task2/Main.java
@@ -0,0 +1,40 @@
+package com.epam.advancedMultithreading.task2;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("quicksort starts");
+		quickSortTest();
+		System.out.println("mergesort starts");
+		mergeSortTest();
+	}
+
+	private static void quickSortTest(){
+		ForkJoinPool fjPool = new ForkJoinPool();
+		int[] a = new int[100000001];
+		for (int i = 0; i < a.length; i++) {
+			int k = (int) (Math.random() * 22222);
+			a[i] = k;
+		}
+		ForkJoinQuicksortTask forkJoinQuicksortTask = new ForkJoinQuicksortTask(a,
+				0, a.length - 1);
+		long start = System.nanoTime();
+		fjPool.invoke(forkJoinQuicksortTask);
+		System.out.println("Time: " + (System.nanoTime() - start) + " nanoseconds");
+	}
+
+	private static void mergeSortTest(){
+		ForkJoinPool fjPool = new ForkJoinPool();
+		int[] a = new int[100000001];
+		for (int i = 0; i < a.length; i++) {
+			int k = (int) (Math.random() * 22222);
+			a[i] = k;
+		}
+		ForkJoinMergeSortTask forkJoinQuicksortTask = new ForkJoinMergeSortTask(a,
+				0, a.length - 1);
+		long start = System.nanoTime();
+		fjPool.invoke(forkJoinQuicksortTask);
+		System.out.println("Time: " + (System.nanoTime() - start) + " nanoseconds");
+	}
+}
