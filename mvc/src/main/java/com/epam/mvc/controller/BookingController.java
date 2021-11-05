5e8fa958ff4aa4dc53dae3a0d98772731e0a173a
Bobur_Zokirov <zokirovbobur93@gmail.com>
2021-11-05 11:42:27 +0500

    init version for mvc is ready for spring boot app

--- mvc/src/main/java/com/epam/mvc/controller/BookingController.java
+++ mvc/src/main/java/com/epam/mvc/controller/BookingController.java
@@ -4,13 +4,13 @@ import com.epam.mvc.booking.BookingFacade;
 import com.epam.mvc.dao.EventEntity;
 import com.epam.mvc.dto.GeneralAnswer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("booking")
 public class BookingController {
 	@Autowired
