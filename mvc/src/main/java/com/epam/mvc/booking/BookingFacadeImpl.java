25aff152652b191ec1abd9e74a235b34aa5dd215
Bobur_Zokirov <zokirovbobur93@gmail.com>
2021-11-07 23:51:23 +0500

    mvc optional task is ready for review

--- mvc/src/main/java/com/epam/mvc/booking/BookingFacadeImpl.java
+++ mvc/src/main/java/com/epam/mvc/booking/BookingFacadeImpl.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -105,6 +106,7 @@ public class BookingFacadeImpl implements BookingFacade{
 	}
 
 	@PostConstruct
+	@Transactional
 	public void preloadTickets(){
 		log.info("creating initial events data");
 		List<EventEntity> eventEntities = EventsInit.convertXMLToEvents();
